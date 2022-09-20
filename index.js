8b52ec9bf2259b5204a9594f521ebc3adc62d5d0
kazuopp312 <kazuopp312@gmail.com>
2022-09-20 18:36:15 +0900

    make server

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const person = require('./person');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-console.log(person.name);
+const server = http.createServer((req, res) => {
+  if(req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Home</h1>')
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
